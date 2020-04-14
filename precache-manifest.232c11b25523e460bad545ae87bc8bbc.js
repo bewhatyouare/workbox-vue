6eb348495ef56126a931199000f7ae99ef4c7eef
hq <smile_to_life2010@126.com>
2020-04-14 17:27:01 +0800

    17

---
+++ precache-manifest.232c11b25523e460bad545ae87bc8bbc.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c5319881f30339efcf93",
+    "url": "css/app.927f4782.css"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "01273ac1acee5bed0c02d640a2de2aef",
+    "url": "index.html"
+  },
+  {
+    "revision": "c5319881f30339efcf93",
+    "url": "js/app.a030e145.js"
+  },
+  {
+    "revision": "0532dc04a348f6eaa5ac",
+    "url": "js/chunk-vendors.a2bfe163.js"
+  },
+  {
+    "revision": "f8f3595a1327327851f72d505e347d0c",
+    "url": "manifest.json"
+  },
+  {
+    "revision": "735ab4f94fbcd57074377afca324c813",
+    "url": "robots.txt"
+  },
+  {
+    "revision": "0b05ee4e7e566779dae0bf84bc3401ce",
+    "url": "sw.js"
+  }
+]);
--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-importScripts("precache-manifest.7edc37a85aa43e66cc5dc6fce4fada8f.js", "https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+importScripts("precache-manifest.232c11b25523e460bad545ae87bc8bbc.js", "https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 
 importScripts('https://storage.googleapis.com/workbox-cdn/releases/3.0.0-alpha.3/workbox-sw.js');
@@ -9,9 +9,15 @@ if (workbox) {
 else {
     console.log(`Boo! workbox didn't load `);
 }
+console.info(workbox);
+console.info(workbox.precaching)
 //precache (预缓存) 静态文件
-workbox.precaching.preacheAndRoute([
-    '/js/app.eec3767d.js'
+workbox.precaching.precacheAndRoute([
+    '/js/app.eec3767d.js',
+    {
+        url:'/index.html',
+        revision:'eec3767d'
+    }
 ]);
 //https://blog.csdn.net/yelin042/article/details/79837745
 
